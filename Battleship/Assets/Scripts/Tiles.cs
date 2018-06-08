cb06ae02529904a23dd0977bc73502c237127c8f
DanniGudna <dannigudna@gmail.com>
2018-06-08 18:32:18 +0000

    byrjun að tilemap scriptu komin
    mappað út öll tiles með script

---
+++ Battleship/Assets/Scripts/Tiles.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class Tiles : MonoBehaviour {
+
+	// Use this for initialization
+	void Start()
+	{
+		Tilemap tilemap = GetComponent<Tilemap> ();
+		tilemap.CompressBounds();
+		BoundsInt bounds = tilemap.cellBounds;
+		TileBase[] allTiles = tilemap.GetTilesBlock(bounds);
+
+		for (int x = 0; x < bounds.size.x; x++) {
+			for (int y = 0; y < bounds.size.y; y++) {
+				TileBase tile = allTiles [x + y * bounds.size.x];
+				if (tile != null) {
+					Debug.Log("x:" + x + " y:" + y + " tile:" + tile.name);
+				} else {
+					Debug.Log("x:" + x + " y:" + y + " tile: (null)");
+				}
+			}
+		}
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+
+
+
+
+
+	}
+}
