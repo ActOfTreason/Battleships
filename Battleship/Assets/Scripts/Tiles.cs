baa71239d003d6e43d9894c21a90a43d9ac695a3
DanniGudna <dannigudna@gmail.com>
2018-06-09 15:43:01 +0000

    bætti við asset frá Unity/hægt að clicka á reiti/ hægt að spawna skip á reitum
    Ná í 2d asset extras pakkann frá Unity og notað þau brush til að spawna game object, basic mouseclicking sem notar coord til að athuga hvaða reit er verið að ýta á

--- Battleship/Assets/Scripts/Tiles.cs
+++ Battleship/Assets/Scripts/Tiles.cs
@@ -1,14 +1,17 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 
 public class Tiles : MonoBehaviour {
 
-	// Use this for initialization
-	void Start()
+
+    Tilemap tilemap;
+    // Use this for initialization
+    void Start()
 	{
-		Tilemap tilemap = GetComponent<Tilemap> ();
+		tilemap = GetComponent<Tilemap> ();
 		tilemap.CompressBounds();
 		BoundsInt bounds = tilemap.cellBounds;
 		TileBase[] allTiles = tilemap.GetTilesBlock(bounds);
@@ -17,9 +20,9 @@ public class Tiles : MonoBehaviour {
 			for (int y = 0; y < bounds.size.y; y++) {
 				TileBase tile = allTiles [x + y * bounds.size.x];
 				if (tile != null) {
-					Debug.Log("x:" + x + " y:" + y + " tile:" + tile.name);
+					//Debug.Log("x:" + x + " y:" + y + " tile:" + tile.name);
 				} else {
-					Debug.Log("x:" + x + " y:" + y + " tile: (null)");
+					//Debug.Log("x:" + x + " y:" + y + " tile: (null)");
 				}
 			}
 		}
@@ -29,7 +32,13 @@ public class Tiles : MonoBehaviour {
 	void Update()
 	{
 
-
+		if (Input.GetMouseButtonDown (0)) {
+			Vector3 pos = Camera.main.ScreenToWorldPoint (Input.mousePosition);
+			Debug.Log ("x: " + pos.x + "y; " + pos.y);
+            Vector3Int posis = new Vector3Int((int)Math.Floor(pos.x), (int)Math.Floor(pos.y), 0);
+            TileBase CLickedTile = tilemap.GetTile(posis);
+            Debug.Log("the tile " + CLickedTile);
+		}
 
 
 
