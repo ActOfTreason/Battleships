d0efa4db0a843b0bb2e4dea26e97ddf03d8cfb5e
DanniGudna <dannigudna@gmail.com>
2018-06-09 19:11:30 +0000

    breytt virkni á mousedown

--- Battleship/Assets/Scripts/Tiles.cs
+++ Battleship/Assets/Scripts/Tiles.cs
@@ -8,13 +8,21 @@ public class Tiles : MonoBehaviour {
 
 
     Tilemap tilemap;
+    Tilemap ships;
+    Transform test;
+    public GameObject debugShip;
+    TileBase[] gameBoard;
     // Use this for initialization
     void Start()
 	{
-		tilemap = GetComponent<Tilemap> ();
-		tilemap.CompressBounds();
+		tilemap = GetComponentInChildren<Tilemap> ();
+        //tilemap = GetComponentInChildren<Tilemap>();
+        test = this.gameObject.transform.GetChild(2);
+        ships = test.GetComponent<Tilemap> ();
+        tilemap.CompressBounds();
 		BoundsInt bounds = tilemap.cellBounds;
 		TileBase[] allTiles = tilemap.GetTilesBlock(bounds);
+        Debug.Log("this " + allTiles);
 
 		for (int x = 0; x < bounds.size.x; x++) {
 			for (int y = 0; y < bounds.size.y; y++) {
@@ -35,9 +43,13 @@ public class Tiles : MonoBehaviour {
 		if (Input.GetMouseButtonDown (0)) {
 			Vector3 pos = Camera.main.ScreenToWorldPoint (Input.mousePosition);
 			Debug.Log ("x: " + pos.x + "y; " + pos.y);
-            Vector3Int posis = new Vector3Int((int)Math.Floor(pos.x), (int)Math.Floor(pos.y), 0);
+            //Vector3Int posis = new Vector3Int((int)Math.Floor(pos.x), (int)(Math.Floor(pos.y) - 0.5), 1);
+            Vector3Int posis = new Vector3Int((int)pos.x, (int)(Math.Floor(pos.y) - 0.5), 1);
+            Debug.Log(posis);
             TileBase CLickedTile = tilemap.GetTile(posis);
             Debug.Log("the tile " + CLickedTile);
+            TileBase spawnTile = ships.GetTile(posis);
+            Instantiate(debugShip, posis, Quaternion.identity);
 		}
 
 
